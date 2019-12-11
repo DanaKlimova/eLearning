774832cce56e6ddd7532c3d9e5092efe52b8dba9
Dana Klimova <dana.klimava@itechart-group.com>
2019-12-11 09:51:12 +0300

    Registered Account in admin.

--- eLearning/accounts/admin.py
+++ eLearning/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import Account
 
-# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ("email", "first_name", "last_name", "is_admin", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    readonly_fields = ()
+    ordering = ("email",)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
