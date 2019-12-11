57baa0cdf9b48c452688839a200c953b76ddcc4f
Dana Klimova <dana.klimava@itechart-group.com>
2019-12-11 15:00:06 +0300

    Changed the way of getting database user name.

--- eLearning/eLearning/settings.py
+++ eLearning/eLearning/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'elearning',
-        'USER': 'dana',
+        'USER': os.environ['USER'],
         'PASSWORD': 'dana',
         'HOST': '127.0.0.1',
         'PORT': '5432',
