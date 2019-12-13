4c8e875edffc02f19f29ebd5f7ceed5b23326f1b
Dana Klimova <dana.klimava@itechart-group.com>
2019-12-13 15:33:59 +0300

    Created validation decorator for user creation func.

--- eLearning/accounts/models.py
+++ eLearning/accounts/models.py
@@ -2,17 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
-class AccountManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, password):
-        if not email:
-            raise ValueError("User must have an email address.")
-        if not first_name:
-            raise ValueError("User must have a fisrt name.")
-        if not last_name:
-            raise ValueError("User must have a last name.")
-        if not password:
-            raise ValueError("User must have a password.")
+def _is_valid_credentials(func):
+        def wrapper(self, email, first_name, last_name, password):
+            if not email:
+                raise ValueError("User must have an email address.")
+            if not first_name:
+                raise ValueError("User must have a fisrt name.")
+            if not last_name:
+                raise ValueError("User must have a last name.")
+            if not password:
+                raise ValueError("User must have a password.")
+            func(email, first_name, last_name, password)
+        return wrapper
 
+
+class AccountManager(BaseUserManager):
+    @_is_valid_credentials
+    def create_user(self, email, first_name, last_name, password):
         user = self.model(
             email=self.normalize_email(email),
             first_name=first_name,
