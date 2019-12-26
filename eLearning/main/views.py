710a30a8136615a76a662b73b7896cf47f6e3202
Dana Klimova <dana.klimava@itechart-group.com>
2019-12-26 13:27:27 +0300

    Fixed problem with Course and CourseEnrollment, made code readable.

--- eLearning/main/views.py
+++ eLearning/main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 from courses.models import Course
 
-
+# TODO: think about course ordering.
 def home(request):
     context = {}
     user = request.user
@@ -15,21 +15,37 @@ def home(request):
             'courses',
         ])
 
-        current_courseenrollment = NamedCourses('Current courses', user.courseenrollment_set.filter(
+        current_course_enrollments = user.courseenrollment_set.filter(
             finished_at__isnull=True
-        ).order_by('-course__rating')[:5])
+        ).order_by('-course__rating')[:5]
+        current_courses = NamedCourses(
+            'Current courses',
+            [enrollment.course for enrollment in current_course_enrollments]
+        )
 
-        current_courses = [enrollment.course for enrollment in current_courseenrollment]
         users_courses = NamedCourses('Users courses', (
             Course.objects.filter(type='pbl', owner_type='usr').order_by('-rating')[:5]
         ).union(
             user.individual_courses.filter(owner_type='usr').order_by('-rating')[:5]
         ))
-        starred_courses = NamedCourses('Starred courses', user.favorite_courses.order_by('-rating')[:5])
-        last_completed_courses = NamedCourses('Last completed courses', user.courseenrollment_set.filter(
+
+        starred_courses = NamedCourses(
+            'Starred courses',
+            user.favorite_courses.order_by('-rating')[:5]
+        )
+
+        last_completed_course_enrollments = user.courseenrollment_set.filter(
             finished_at__isnull=False
-        ).order_by('-course__rating')[:5])
-        recomended_courses = NamedCourses('Recomended', Course.objects.order_by('-rating')[:5])
+        ).order_by('-course__rating')[:5]
+        last_completed_courses = NamedCourses(
+            'Last completed courses',
+            [enrollment.course for enrollment in last_completed_course_enrollments]
+        )
+
+        recomended_courses = NamedCourses(
+            'Recomended',
+            Course.objects.order_by('-rating')[:5]
+        )
 
         context['courses_set'] = [
             current_courses,
