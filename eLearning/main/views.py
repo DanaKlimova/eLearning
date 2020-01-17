5b69b7aafd82718b6c207bb36898e15b881ed231
Dana Klimova <dana.klimava@itechart-group.com>
2020-01-17 19:05:17 +0300

    Changed amount of courses on the home page. Changed style of the base template. Changed layout of the homa page.

--- eLearning/main/views.py
+++ eLearning/main/views.py
@@ -17,26 +17,26 @@ def home(request):
 
         current_course_enrollments = user.courseenrollment_set.filter(
             finished_at__isnull=True
-        ).order_by('-course__rating')[:5]
+        ).order_by('-course__rating')[:6]
         current_courses = NamedCourses(
             'Current courses',
             [enrollment.course for enrollment in current_course_enrollments]
         )
 
         users_courses = NamedCourses('Users courses', (
-            Course.objects.filter(type='pbl', owner_type='usr', status='rdy').order_by('-rating')[:5]
+            Course.objects.filter(type='pbl', owner_type='usr', status='rdy').order_by('-rating')[:6]
         ).union(
-            user.individual_courses.filter(owner_type='usr', status='rdy').order_by('-rating')[:5]
+            user.individual_courses.filter(owner_type='usr', status='rdy').order_by('-rating')[:6]
         ))
 
         starred_courses = NamedCourses(
             'Starred courses',
-            user.favorite_courses.order_by('-rating')[:5]
+            user.favorite_courses.order_by('-rating')[:6]
         )
 
         last_completed_course_enrollments = user.courseenrollment_set.filter(
             finished_at__isnull=False
-        ).order_by('-course__rating')[:5]
+        ).order_by('-course__rating')[:6]
         last_completed_courses = NamedCourses(
             'Last completed courses',
             [enrollment.course for enrollment in last_completed_course_enrollments]
@@ -44,7 +44,7 @@ def home(request):
 
         recomended_courses = NamedCourses(
             'Recomended',
-            Course.objects.filter(type='pbl', status='rdy').order_by('-rating')[:5]
+            Course.objects.filter(type='pbl', status='rdy').order_by('-rating')[:6]
         )
 
         context['courses_set'] = [
