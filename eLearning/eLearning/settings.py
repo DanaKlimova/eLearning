70a294710c38156ab89f844997ae6bc23c5ab6e6
Dana Klimova <dana.klimava@itechart-group.com>
2020-01-29 14:02:04 +0300

    Added db setting after heroku settings.

--- eLearning/eLearning/settings.py
+++ eLearning/eLearning/settings.py
@@ -179,4 +179,18 @@ LOGGING = {
     },
 }
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
+}
