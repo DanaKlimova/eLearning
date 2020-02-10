b2736c821eaacb05441f14757d0e3d086335195f
Dana Klimova <dana.klimava@itechart-group.com>
2020-02-10 12:55:01 +0300

    Fixed certificate styles for print and footer. Refactored func for pdf creation.

--- eLearning/courses/utils.py
+++ eLearning/courses/utils.py
@@ -1,29 +1,16 @@
 from io import BytesIO
+import os
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.conf import settings
+from django.contrib.staticfiles import finders
 
 from weasyprint import HTML, CSS
 
 def render_to_pdf(template_src, request, context_dict={}):
     template = get_template(template_src)
     rendered_html = template.render(context_dict)
-
     base_url=request.build_absolute_uri()
-
-    print(settings.BASE_DIR)
-    # html = HTML(string=html_string, base_url=request.build_absolute_uri())
-
-    # pdf_file = HTML(string=rendered_html).write_pdf(stylesheets=[CSS('/home/dana/apps/eLearning_System/eLearning/eLearning/courses/static/courses/css/certificate.css')])
-    pdf_file = HTML(string=rendered_html, base_url=request.build_absolute_uri()).write_pdf(stylesheets=[CSS('/home/dana/apps/eLearning_System/eLearning/eLearning/courses/static/courses/css/certificate.css')])
-
-    http_response = HttpResponse(pdf_file, content_type='application/pdf')
-    http_response['Content-Disposition'] = 'filename="report.pdf"'
-
+    html = HTML(string=rendered_html, base_url=request.build_absolute_uri())
+    pdf_file = html.write_pdf(stylesheets=[CSS(finders.find('courses/css/certificate.css'))])
     return pdf_file
-
-    # result = BytesIO()
-    # pdf = pisa.pisaDocument(BytesIO(html.encode("utf-8")), result)
-    # if not pdf.err:
-    #     return HttpResponse(result.getvalue(), content_type='application/pdf')
-    # return None
