8f488720bb65e867deaae54b1295d876926304e0
DanaKlimova <danonklimova@gmail.com>
2020-05-27 07:23:35 +0300

    Register organization in admin panel.

--- eLearning/accounts/admin.py
+++ eLearning/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import Account
+from accounts.models import Account, Organization
 
 
 class AccountAdmin(UserAdmin):
@@ -14,4 +14,18 @@ class AccountAdmin(UserAdmin):
     fieldsets = ()
 
 
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "employees",
+        "is_visible",
+    )
+    readonly_fields = (
+        "name",
+        "employees",
+    )
+    ordering = ("is_visible",)
+
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(Organization, OrganizationAdmin)
